bf0e5810c2361d096191a61eb3118ef9bc7ee461
bhanukonakala <bhanukonakala45@gmail.com>
2025-04-21 22:57:17 +0530

    Add files via upload

---
+++ reversestring.py
@@ -0,0 +1,15 @@
+string = input("enter a string: ").split()[::-1]
+print(" ".join (string))
+
+
+
+
+
+
+
+
+#input: enter a string:  i am going home
+#output: home going am i
+
+#input: enter a string: this is a laptop
+#output: laptop a is this
